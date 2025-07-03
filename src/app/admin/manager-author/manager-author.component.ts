bd49fe760459651da2cb61328040247994e9d54d
Aliny Souza Ramos <souzaramosaliny@gmail.com>
2025-07-02 22:16:22 -0400

    feat: All the body of the 7 screens was created

---
+++ src/app/admin/manager-author/manager-author.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-author.component',
+  imports: [],
+  templateUrl: './manager-author.component.html',
+  styleUrl: './manager-author.component.scss'
+})
+export class ManagerAuthorComponent {
+
+}
---
+++ src/app/author/create-news/create-news.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-news.component',
+  imports: [],
+  templateUrl: './create-news.component.html',
+  styleUrl: './create-news.component.scss'
+})
+export class CreateNewsComponent {
+
+}
