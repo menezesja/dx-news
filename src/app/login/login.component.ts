58038232a6e9d5c63c72c0cf58b2c7534879a563
Jade Santos <santmjad@gmail.com>
2025-07-29 21:10:14 -0400

    feat: add password visibility toggle and import Angular Material

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { AlertDialogComponent } from '../feedback/alert-dialog.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatDialogModule],
+  imports: [CommonModule, FormsModule, MatDialogModule, MatCardModule,
+            MatFormFieldModule,MatInputModule,MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  hide = signal(true);
 
   constructor(
     private authService: AuthService, 
@@ -34,4 +41,10 @@ export class LoginComponent {
       }
     });
   }
+
+  togglePasswordVisibility(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
 }
