3a55bed6f6e8eb4c347f04ef4a099612726cb0f9
Aliny Ramos <aliny.souz.r@gmail.com>
2025-07-31 16:12:14 -0400

    fix: change how call the ManagerAdmin

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
-import { ManagerAuthorComponent } from '../admin/manager-author/manager-author.component'; // Importe este
+import { ManagerAuthor } from '../admin/manager-author/manager-author.component'; // Importe este
 import { ManagerNewsComponent } from '../admin/manager-news/manager-news.component';     // Importe este
 import { CreateNewsComponent } from '../author/create-news/create-news.component';
 import { ControllerNewsComponent } from '../author/controller-news/controller-news.component';
@@ -11,7 +11,7 @@ import { RegistrationComponent } from '../registration/registration.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'admin/authors', component: ManagerAuthorComponent }, // Nova rota para autores
+  { path: 'admin/authors', component: ManagerAuthor }, // Nova rota para autores
   { path: 'admin/news', component: ManagerNewsComponent },    
   { path: 'author/create-news', component: CreateNewsComponent },    
   { path: 'author/create-news/edit/:id', component: CreateNewsComponent },    
